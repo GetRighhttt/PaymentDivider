a120dc1a79b059a9fc55c41db5e2c51402d843b3
Stefan Bayne <stefanbayne@gmail.com>
2022-11-02 08:57:22 -0400

    Initial commit

---
+++ PaymentDivider/PaymentDividerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentDividerApp.swift
+//  PaymentDivider
+//
+//  Created by Stefan Bayne on 11/2/22.
+//
+
+import SwiftUI
+
+@main
+struct PaymentDividerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
